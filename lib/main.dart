ed93127856190c665135e52f82e9a1865b008c08
Prottoy Kundu <163542890+prottoykundu38@users.noreply.github.com>
2025-10-12 18:14:03 +0600

    added crashlytics and analytics

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:e_commerce_app/app/app.dart';
+import 'package:e_commerce_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+//localization
+//theme
+//crashlytics
+//analytics
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  //framework error
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+  runApp(const CraftyBay());
+}
