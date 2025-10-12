f48390d5c300493aaab0ae7e3a264ada4e328503
Prottoy Kundu <163542890+prottoykundu38@users.noreply.github.com>
2025-10-12 23:22:55 +0600

    localization added

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,7 @@
+import 'package:e_commerce_app/l10n/app_localizations.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class CraftyBay extends StatefulWidget {
   const CraftyBay({super.key});
@@ -15,7 +17,18 @@ class _CraftyBayState extends State<CraftyBay> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       navigatorObservers: [observer],
+      locale: Locale('en'),
+      supportedLocales: [
+        Locale('en'), // English
+        Locale('bn'), // Spanish
+      ],
     );
   }
 }
